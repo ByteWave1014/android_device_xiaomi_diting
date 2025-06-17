3d2687fc6251e175c735d335e6e060b90ee7760b
lolipuru <banstellazz@gmail.com>
2025-06-18 00:08:47 +0700

    diting: Add euicc overlay for global variant
    Change-Id: Ic7520e8b141c7289d2d1fdf2967f17ac42ab3edf
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResDiting \
+    EuiccResDiting \
     FrameworksResDiting \
     NfcResDiting \
     SettingsProviderResDiting \
